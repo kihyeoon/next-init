4430be9ccff67c0d1cacb9ae16f044fddd034042
kihyeoon <bangion93@gmail.com>
2023-05-09 23:57:05 +0900

    4.9 페이지 미리 생성

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,3 +7,8 @@ interface Props {
 export default function DetailPage({ params: { slug } }: Props) {
   return <h1>{slug} 제품 설명 페이지</h1>;
 }
+
+export function generateStaticParams() {
+  const products = ["pants", "shirts", "socks"];
+  return products.map((product) => ({ slug: product }));
+}
