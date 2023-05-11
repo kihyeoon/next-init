938f4f3f0126b9700f289dbf677caf7a7c9ef91a
kihyeoon <bangion93@gmail.com>
2023-05-11 23:20:02 +0900

    5.7 리팩토링 (설계)

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from "@/service/products";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
@@ -14,18 +15,22 @@ export function generateMetadata({ params: { slug } }: Props) {
 }
 
 export default function DetailPage({ params: { slug } }: Props) {
-  if (slug === "nothing") notFound();
+  //todo: 서버 파일(데이터베이스)에 있는 제품 정보를 읽어와서 보여준다.
+  const product = getProduct(slug);
+
+  if (!product) notFound();
 
   return (
     <>
-      <h1>{slug} 제품 설명 페이지</h1>
-      <p>{slug} 제품 설명</p>
+      <h1>{product} 제품 설명 페이지</h1>
+      <p>{product} 제품 설명</p>
       <Link href="/products">목록으로 이동</Link>
     </>
   );
 }
 
 export function generateStaticParams() {
-  const products = ["pants", "shirts", "socks"];
+  //todo: 모든 제품의 페이지들을 미리 만들어 둔다.(SSG)
+  const products = getProducts();
   return products.map((product) => ({ slug: product }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,10 @@
+import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-const products = ["pants", "shirts", "skirt", "shoes"];
-
 export default function ProductsPage() {
+  //todo: 서버 파일(데이터베이스)에 있는 제품 리스트를 읽어와서 보여준다.
+  const products = getProducts();
+
   return (
     <>
       <h1>ProductsPage</h1>
