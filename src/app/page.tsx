562586cd983b2d55e91eee1f30a55d8e6ab4a8e5
kihyeoon <bangion93@gmail.com>
2023-05-13 23:33:38 +0900

    6.5 이미지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Counter from "@/components/Counter";
-import os from "os"; // Node.js API
+import Image from "next/image";
 
 export default function Home() {
   console.log("hi! - server");
-  console.log(os.hostname());
 
   return (
     <>
       <h1>홈페이지!!</h1>
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="shop"
+        width={700}
+        height={500}
+      />
       <Counter />
     </>
   );
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,15 @@
 import MeowArticle from "@/components/MeowArticle";
 import { getProducts } from "@/service/products";
 import Link from "next/link";
+import Image from "next/image";
+import clothesImage from "public/images/clothes.jpg";
 
 export default async function ProductsPage() {
-  throw new Error("Error!");
   const products = await getProducts();
   return (
     <>
       <h1>ProductsPage</h1>
+      <Image src={clothesImage} alt="clothes" priority />
       <ul>
         {products.map(({ id, name }) => (
           <li key={id}>
