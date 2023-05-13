4a88ee100b4eec7c253e36cd6d479dacf722fd7e
kihyeoon <bangion93@gmail.com>
2023-05-14 03:14:24 +0900

    6.8 폰트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 import styles from "./layout.module.css";
 import "./globals.css";
 import { Metadata } from "next";
+import { Open_Sans } from "@next/font/google";
+import { Nanum_Gothic } from "@next/font/google";
+
+const sans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({
+  subsets: ["latin"],
+  weight: "700",
+});
 
 export const metadata: Metadata = {
   title: "멋진 제품 사이트",
@@ -17,10 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       <body>
         <header className={styles.header}>
-          <h1>Demo Note</h1>
+          <h1 className={gothic.className}>Demo Note</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
             <Link href="/contact">Contact</Link>
