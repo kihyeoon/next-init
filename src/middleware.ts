62bccefcc7649aa796d38f65ca04a99e3eba5495
kihyeoon <bangion93@gmail.com>
2023-05-14 15:49:03 +0900

    6.12 미들웨어

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log("미들웨어 실행중!");
+  if (request.nextUrl.pathname.startsWith("/products/1004")) {
+    console.log("경로 변경!");
+    return NextResponse.redirect(new URL("products", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/products/:path+"],
+};
